7fc17dd5441e95e70e4398db1ffb438e8e93e10e
Aditya Mane <112119500+ADITYA0920@users.noreply.github.com>
2023-06-14 12:31:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
 
+	let f_name = document.getElementById("fname").value ;
+	let l_name = document.getElementById("lname").value ;
+
+	alert(f_name+" "+l_name) ;
+
 }
+
